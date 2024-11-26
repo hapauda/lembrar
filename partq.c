27db74508d7b6254668cb87785d80bc29bb41f5a
Ademir Vieira <vieiraademir707@gmail.com>
2024-11-26 15:51:31 -0300

    parte de exercicios

--- partq.c
+++ partq.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
 
-int main() {
-  // Create an int and a char variable
-  int myNum;
-  char myChar [20] ;
-
-  // Ask the user to type a number AND a character
-  printf("Type a number AND a character and press enter: \n");
-
-  // Get and save the number AND character the user types
-  scanf("%d %s", &myNum, myChar);
-
-  // Print the number
-  printf("Your number is: %d, char is: %s\n", myNum, myChar);
-
-  // Print the character
-  
-  return 0;
+int main(){
+  int *p;
+  p = (int *) malloc(5*sizeof(int));
+  printf("%d\n", p);
+  printf("Holas amigo\n");
+  free(p);
 }
